3f86cfeddb38db687088eee1a17a29946b928628
doho <zhaodonghao586@outlook.com>
2020-04-20 15:23:50 +0800

    init

---
+++ components/parts/a.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const Link: React.SFC = props => {
+    return <div>
+        Link
+        {props.children}
+    </div>
+}
---
+++ components/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: "./index.tsx",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+    },
+    devtool: "source-map",
+}
